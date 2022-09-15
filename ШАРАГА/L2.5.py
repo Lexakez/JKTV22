cb71fec5fca1fca782b0d9b0fe7919d00353be93
Lexakez <lexakez@gmail.com>
2022-09-16 01:35:35 +0300

    Create

---
+++ ШАРАГА/L2.5.py
@@ -0,0 +1,19 @@
+from math import ceil
+
+c1 = int(input("Students in first class: "))
+c2 = int(input("Students in second class: "))
+c3 = int(input("Students in third class: "))
+
+# Первый вариант
+
+bc1 = (c1 // 2) + (c1 % 2) \
+      + (c2 // 2) + (c2 % 2) \
+      + (c3 // 2) + (c3 % 2)
+
+print(f"Парт надо закупить {bc1}")
+
+# Второй вариант
+
+bc2 = (ceil(c1 / 2)) + (ceil(c1 / 2)) + (ceil(c1 / 2))
+
+print(f"Парт надо закупить: {bc2}")
