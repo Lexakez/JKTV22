224bc5647a0a82ecaaf9b627a522260725b99c01
Lexakez <lexakez@gmail.com>
2022-09-16 20:56:42 +0300

    lesson

--- ШАРАГА/L3.4.py
+++ ШАРАГА/L3.4.py
@@ -1,4 +1,5 @@
 from math import ceil
+
 carkm = int(input("Введите сколько киллометров в день может проехать машина:"))
 
 km = int(input("Введите длинну маршрута в километрах:"))
@@ -7,5 +8,16 @@ days = km / carkm
 
 days = ceil(days)
 
-print(f"машина проедет это растояние за: {days} days")
+pad = days % 100
+
+if pad >= 10 and pad <= 20:
+    print(f"Машина проедет это растояние за {days} дней")
+elif pad < 10 or pad > 20:
+    pad %= 10
+    if pad == 1:
+        print(f"Машина проедет это растояние за {days} день")
+    elif pad > 1 and pad < 5:
+        print(f"Машина проедет это растояние за {days} дня")
+    else:
+        print(f"Машина проедет это растояние за {days} дней")
 
