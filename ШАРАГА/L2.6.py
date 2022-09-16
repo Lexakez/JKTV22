fec0df5420e77f4c8ad7d8375c3e0393c7dc29f1
Lexakez <lexakez@gmail.com>
2022-09-16 16:44:06 +0300

    lesson

--- ШАРАГА/L2.6.py
+++ ШАРАГА/L2.6.py
@@ -1,1 +1,15 @@
-k
+time = int(input("Введите время в минутах: "))
+
+time %= 1440
+
+hours = time // 60
+
+minutes = time % 60
+
+if minutes < 10:
+    minutes = "0" + str(minutes)
+
+if hours < 10:
+    hours = "0" + str(hours)
+
+print(f"{hours}:{minutes}")
