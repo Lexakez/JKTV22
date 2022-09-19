273f8fde6d4c123f7b08464616d1bc056d395863
Lexakez <113642401+Lexakez@users.noreply.github.com>
2022-09-19 12:15:29 +0300

    Update casino.py

--- My files/casino.py
+++ My files/casino.py
@@ -121,84 +121,87 @@ ballance = 1000
 
 
 while ballance > 0:
-    bet_amount = int(input(f"You have {ballance}$ \nHow much you want to bet: "))
+    bet_amount = abs(int(input(f"You have {ballance}$ \nHow much you want to bet: ")))
 
-    ballance -= bet_amount
+
 
     print(f"Your bet: {bet_amount}$")
+    if bet_amount <= ballance:
+        ballance -= bet_amount
+        print("List of position: \n\
+            1. Numbers (from 0 to 36) \n\
+            2. Even or Odd\n\
+            3. Which line (From Line 1 to Line 3 \n\
+            4. Which third (From Third 1 to Third 3 \n\
+            5. Which colour (Red or Black) \n\
+            6. Which half(Half 1 or Half 2)")
 
-    print("List of position: \n\
-        1. Numbers (from 0 to 36) \n\
-        2. Even or Odd\n\
-        3. Which line (From Line 1 to Line 3 \n\
-        4. Which third (From Third 1 to Third 3 \n\
-        5. Which colour (Red or Black) \n\
-        6. Which half(Half 1 or Half 2)")
+        ruletka = randint(0, 36)
 
-    ruletka = randint(0, 36)
+        bet_pos = input(f"In which position you want to bet: ")
 
-    bet_pos = input(f"In which position you want to bet: ")
+        if bet_pos.isnumeric():
+            ballance = check_bet_add_ballance(bet_amount, ballance, 36, ruletka == int(bet_pos))
+            if not is_green(ruletka):
+                print(f"Roulette: {ruletka}")
 
-    if bet_pos.isnumeric():
-        ballance = check_bet_add_ballance(bet_amount, ballance, 36, ruletka == int(bet_pos))
         if not is_green(ruletka):
-            print(f"Roulette: {ruletka}")
+            if bet_pos.lower() == "even":
+                ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_even(ruletka))
+                if is_even(ruletka):
+                    print(f"Roulette: {ruletka} Even")
+                else:
+                    print(f"Roulette: {ruletka} Odd")
 
-    if not is_green(ruletka):
-        if bet_pos.lower() == "even":
-            ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_even(ruletka))
-            if is_even(ruletka):
-                print(f"Roulette: {ruletka} Even")
-            else:
-                print(f"Roulette: {ruletka} Odd")
+            if bet_pos.lower() == "odd":
+                ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_odd(ruletka))
+                if is_odd(ruletka):
+                    print(f"Roulette: {ruletka} Odd")
+                else:
+                    print(f"Roulette: {ruletka} Even")
 
-        if bet_pos.lower() == "odd":
-            ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_odd(ruletka))
-            if is_odd(ruletka):
-                print(f"Roulette: {ruletka} Odd")
-            else:
-                print(f"Roulette: {ruletka} Even")
+            if bet_pos[0:4].lower() == "line":
+                ballance = check_bet_add_ballance(bet_amount, ballance, 3, is_right_line(int(bet_pos[-1]), ruletka))
+                if in_which_line(ruletka) == 1:
+                    print(f"Roulette: {ruletka} First line")
+                elif in_which_line(ruletka) == 2:
+                    print(f"Roulette: {ruletka} Second line")
+                else:
+                    print(f"Roulette: {ruletka} Third line")
 
-        if bet_pos[0:4].lower() == "line":
-            ballance = check_bet_add_ballance(bet_amount, ballance, 3, is_right_line(int(bet_pos[-1]), ruletka))
-            if in_which_line(ruletka) == 1:
-                print(f"Roulette: {ruletka} First line")
-            elif in_which_line(ruletka) == 2:
-                print(f"Roulette: {ruletka} Second line")
-            else:
-                print(f"Roulette: {ruletka} Third line")
+            if bet_pos[0:5].lower() == "third":
+                ballance = check_bet_add_ballance(bet_amount, ballance, 3, is_right_third(int(bet_pos[-1]), ruletka))
+                if in_which_third(ruletka) == 1:
+                    print(f"Roulette: {ruletka} First third")
+                elif in_which_third(ruletka) == 2:
+                    print(f"Roulette: {ruletka} Second third")
+                else:
+                    print(f"Roulette: {ruletka} Third third")
 
-        if bet_pos[0:5].lower() == "third":
-            ballance = check_bet_add_ballance(bet_amount, ballance, 3, is_right_third(int(bet_pos[-1]), ruletka))
-            if in_which_third(ruletka) == 1:
-                print(f"Roulette: {ruletka} First third")
-            elif in_which_third(ruletka) == 2:
-                print(f"Roulette: {ruletka} Second third")
-            else:
-                print(f"Roulette: {ruletka} Third third")
+            if bet_pos.lower() == "red":
+                ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_red(ruletka))
+                if is_red(ruletka):
+                    print(f"Roulette: {ruletka} Red")
+                else:
+                    print(f"Roulette: {ruletka} Black")
 
-        if bet_pos.lower() == "red":
-            ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_red(ruletka))
-            if is_red(ruletka):
-                print(f"Roulette: {ruletka} Red")
-            else:
-                print(f"Roulette: {ruletka} Black")
+            if bet_pos.lower() == "black":
+                ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_black(ruletka))
+                if is_red(ruletka):
+                    print(f"Roulette: {ruletka} Red")
+                else:
+                    print(f"Roulette: {ruletka} Black")
 
-        if bet_pos.lower() == "black":
-            ballance = check_bet_add_ballance(bet_amount, ballance, 2, is_black(ruletka))
-            if is_red(ruletka):
-                print(f"Roulette: {ruletka} Red")
-            else:
-                print(f"Roulette: {ruletka} Black")
-
-        if bet_pos[0:4].lower() == "half":
-            if int(bet_pos[-1]) == 1:
-                ballance = check_bet_add_ballance(bet_amount, ballance, 2, in_first_half(ruletka))
-            elif int(bet_pos[-1]) == 2:
-                ballance = check_bet_add_ballance(bet_amount, ballance, 2, in_second_half(ruletka))
-            if in_first_half(ruletka):
-                print(f"Roulette: {ruletka} First half")
-            else:
-                print(f"Roulette: {ruletka} Second half")
+            if bet_pos[0:4].lower() == "half":
+                if int(bet_pos[-1]) == 1:
+                    ballance = check_bet_add_ballance(bet_amount, ballance, 2, in_first_half(ruletka))
+                elif int(bet_pos[-1]) == 2:
+                    ballance = check_bet_add_ballance(bet_amount, ballance, 2, in_second_half(ruletka))
+                if in_first_half(ruletka):
+                    print(f"Roulette: {ruletka} First half")
+                else:
+                    print(f"Roulette: {ruletka} Second half")
+        else:
+            print(f"Roulette {ruletka} Its green!")
     else:
-        print(f"Roulette {ruletka} Its green!")
+        print("The bet is not correct, try again.")
