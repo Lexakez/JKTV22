180b52097c2fe8d25f552316c9ec32a0eaa6871e
Ilja Lobossok <iljalobossok@gmail.com>
2022-09-30 22:36:33 +0300

    adjust ABC points

--- My files/треугольник Серпинского.py
+++ My files/треугольник Серпинского.py
@@ -1,10 +1,11 @@
 import pygame
 from random import randint
+
 pygame.init()
 window = pygame.display.set_mode((1900, 1000))
 
 test = True
-A, B, C = (900, 200), (600, 700), (1200, 700)
+A, B, C = (900, 0), (300, 1000), (1500, 1000)
 top_list = [A, B, C]
 start_point = A
 point_list = []
@@ -14,14 +15,17 @@ while test:
     for top in top_list:
         pygame.draw.circle(window, (0, 255, 0), top, 2)
     end_point = top_list[randint(0, 2)]
-    end_point = ((end_point[0] + start_point[0]) / 2, (end_point[1] + start_point[1]) / 2)
+    end_point = (
+        (end_point[0] + start_point[0]) / 2,
+        (end_point[1] + start_point[1]) / 2,
+    )
     end_point += ((randint(0, 255), randint(0, 255), randint(0, 255)),)
     point_list.append(end_point)
     start_point = end_point
     for point in point_list:
-        pygame.draw.circle(window, point[2], (point[0], point[1]), 1)  
+        pygame.draw.circle(window, point[2], (point[0], point[1]), 1)
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                test = False
-    pygame.display.update() 
+        if event.type == pygame.QUIT:
+            test = False
+    pygame.display.update()
 pygame.quit()
