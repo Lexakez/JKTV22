c56869e9c5a88996aebcc49dd01949f638bac2f1
Lexakez <113642401+Lexakez@users.noreply.github.com>
2022-10-28 09:52:38 +0300

    Add files via upload

---
+++ IVKHK/Python/Def praktika/def praktikum.py
@@ -0,0 +1,41 @@
+from math import *
+
+'''
+Задача 1
+'''
+
+# x1 = int(input("Введите x1: "))
+# y1 = int(input("Введите y1: "))
+# x2 = int(input("Введите x2: "))
+# y2 = int(input("Введите y2: "))
+
+# def distance(x1, y1, x2, y2: int) -> int:
+#     dlina = round(sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
+#     return dlina
+
+# print(distance(x1, y1, x2, y2))
+
+'''
+Задача 2
+'''
+
+# a = int(input("Введите число: "))
+# n = int(input("Введите степень числа: "))
+
+# def power(a, n: int) -> int:
+#     a1 = a
+#     for num in range(n - 1):
+#         a1 = a * a1
+#     return a1
+    
+# print(power(a, n))
+
+'''
+Задача 3
+'''
+
+# txt = input("Введите текст: ")
+# def capitalize(txt: str) -> str:
+#     return txt.title()
+# print(capitalize(txt))
+
