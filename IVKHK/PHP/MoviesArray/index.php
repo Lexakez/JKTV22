ae23d7c59408de147c91b68911099b61717a26e7
Lexakez <113642401+Lexakez@users.noreply.github.com>
2022-12-15 14:19:21 +0200

    Add files via upload

--- IVKHK/PHP/MoviesArray/index.php
+++ IVKHK/PHP/MoviesArray/index.php
@@ -1,24 +1,32 @@
 <style>
 .film{
     float: left;
-    width: 400px;
+    width: 450px;
+    height: 600px;
     border: 1px solid black;
     box-shadow: 2px 2px 20px rgb(100, 100, 100);
     padding: 20px;
     margin: 20px;
 }
 .film_description{
-    width: 200px;
+    width: 250px;
     float: left;
 
 }
 .film_description p{
     margin: 6;
 }
+h3{
+    float: left;
+}
+.actors{
+    width: 450px;
+    overflow: hidden;
+}
 .actors_div{
-    margin-top: -70;
     float: left;
     height: 200px;
+    width: 180px;
     border: 1px solid black;
     overflow: auto;
 }
@@ -40,7 +48,9 @@ foreach ($movies as $movie_name => $info){
     $text.='<p> Country: '.$info['country'].'</p>';
     $text.='<p> Description: '.$info['description'].'</p>';
     $text.='</div>';
-    $text.='<h3 style="float: left; margin-top: -100;"> Actors: </h3>';
+    $text.='<div class="actors">';
+    $text.='<hr>';
+    $text.='<h3> Actors: </h3>';
     $text.='<div class="actors_div">';
     foreach ($info['actors'] as $actor){
         $text.='<img src="Images/actors/'.$actor['photo'].'" style="float: left; width: 150px;">';
@@ -48,8 +58,9 @@ foreach ($movies as $movie_name => $info){
     }
     $text.='</div>';
     $text.='</div>';
-}
+    $text.='</div>';
+};
+$text.='<footer><p>Created by Kezerev JKTV22 2022</p></footer>';
 
 echo $text;
 ?>
-<footer><p>Created by Kezerev JKTV22 2022</p></footer>
--- IVKHK/PHP/MoviesArray/movies.php
+++ IVKHK/PHP/MoviesArray/movies.php
@@ -23,26 +23,71 @@ $movies = array (
                 'photo' => 'Leto.png')
         )
     ),
-    '' => array(
-        'tittle' => '',
-        'director' => '',
-        'year' => '',
-        'country' => '',
-        'description' => '',
-        'photo' => '',
+    'The Green Mile' => array(
+        'tittle' => 'The Green Mile',
+        'director' => 'Darabont Ferenc',
+        'year' => '1999',
+        'country' => 'USA',
+        'description' => 'Обвиненный в страшном преступлении, Джон Коффи оказывается в блоке смертников тюрьмы «Холодная гора». Вновь прибывший обладал поразительным ростом и был пугающе спокоен, что, впрочем, никак не влияло на отношение к нему начальника блока Пола Эджкомба, привыкшего исполнять приговор.',
+        'photo' => 'GreenMiles.png',
         'actors' => array(
             array (
-                'name' => '',
-                'photo' => ''),
+                'name' => 'Michael Clarke Duncan',
+                'photo' => 'Duncan.png'),
             array (
-                'name' => '',
-                'photo' => ''),
+                'name' => 'Thomas Jeffrey Hanks',
+                'photo' => 'Hanks.png'),
             array (
-                'name' => '',
-                'photo' => ''),
+                'name' => 'David Bowditch Morse',
+                'photo' => 'Morse.png'),
             array (
-                'name' => '',
-                'photo' => '')
+                'name' => 'Sam Rockwell',
+                'photo' => 'Rockwell.png')
         )
+    ),
+    'The Shawshank Redemption' => array(
+        'tittle' => 'The Shawshank Redemption',
+        'director' => 'Darabont Ferenc',
+        'year' => '1994',
+        'country' => 'USA',
+        'description' => 'Бухгалтер Энди Дюфрейн обвинён в убийстве собственной жены и её любовника. Оказавшись в тюрьме под названием Шоушенк, он сталкивается с жестокостью и беззаконием, царящими по обе стороны решётки. Каждый, кто попадает в эти стены, становится их рабом до конца жизни. Но Энди, обладающий живым умом и доброй душой, находит подход.',
+        'photo' => 'Shawshank.png',
+        'actors' => array(
+            array (
+                'name' => 'Morgan Freeman',
+                'photo' => 'Freeman.png'),
+            array (
+                'name' => 'Timothy Francis Robbins',
+                'photo' => 'Robbins.png'),
+            array (
+                'name' => 'Clarence John Brown',
+                'photo' => 'Brown.png'),
+            array (
+                'name' => 'Robert Patrick Gunton',
+                'photo' => 'Gunton.png')
+        )
+    ),
+    'Igla' => array(
+        'tittle' => 'Игла',
+        'director' => 'Рашид Мусаевич Нугманов',
+        'year' => '1988',
+        'country' => 'USSR',
+        'description' => 'Вернувшись в Алма-Ату, Моро не хочет, чтобы родители знали о его приезде, и в поисках временного пристанища вспоминает о девушке, которую он когда-то любил. Дина рада встрече, но Моро что-то настораживает в ней. Наблюдая за ее странными знакомыми, Моро понимает, что Дина стала наркоманкой. Как спасти девушку?',
+        'photo' => 'Igla.png',
+        'actors' => array(
+            array (
+                'name' => 'Ви́ктор Ро́бертович Цой',
+                'photo' => 'Tsoy.png'),
+            array (
+                'name' => 'Марина Смирнова',
+                'photo' => 'Smirnova.png'),
+            array (
+                'name' => 'Алекса́ндр Никола́евич Баши́ров',
+                'photo' => 'Bashirov.png'),
+            array (
+                'name' => 'Пётр Мамонов',
+                'photo' => 'Mamonov.png')
+        )
+    ),
 );
 ?>
