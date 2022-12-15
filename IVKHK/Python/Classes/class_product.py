c75c5fa4aaf6bd5638b4b30c621ee6d99f5207f2
Lexakez <113642401+Lexakez@users.noreply.github.com>
2022-12-15 11:09:10 +0200

    Add files via upload

---
+++ IVKHK/Python/Classes/class_product.py
@@ -0,0 +1,31 @@
+class Product:
+    def __init__(self, name, code):
+        self.__name = name
+        self.__price = code
+    '''Установка имени'''
+    @property
+    
+    def name(self):
+        return self.__price
+
+    @name.setter
+
+    def name(self, name):
+        self.__name = name
+
+    '''Установка цены'''
+    @property
+
+    def price(self):
+        return self.__price
+    
+    @price.setter
+
+    def price(self, price):
+        if price > 0:
+            self.__price = price
+        else:
+            print("Не правильная цена продукта")
+
+    def print_info(self):
+        print(f"Name: {self.__name}\t|\tPrice: {self.__price}")
---
+++ IVKHK/Python/Classes/product.py
@@ -0,0 +1,9 @@
+from class_product import Product
+
+tomato = Product()
+
+tomato.name = "Tomato"
+
+tomato.price = 10
+
+tomato.print_info()
