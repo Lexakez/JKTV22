70b102cd22a5632002c3f46d01f79f668a3e2dcb
Lexakez <113642401+Lexakez@users.noreply.github.com>
2022-12-16 15:09:06 +0200

    Add files via upload

---
+++ IVKHK/PHP/Contact_Form/footer.php
@@ -0,0 +1,9 @@
+        </article>
+    </div>
+    <footer>
+        <hr>
+        <p>&copy; 2020 IVKHK </p>
+    </footer>
+</div>
+</body>
+</html> 
---
+++ IVKHK/PHP/Contact_Form/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title> send message e-post</title>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    </head>
+</html>
