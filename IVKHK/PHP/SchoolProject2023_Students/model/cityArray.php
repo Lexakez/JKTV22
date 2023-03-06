ed4662a199021da92eb6a2821655af2bbcef7520
Lexakez <113642401+Lexakez@users.noreply.github.com>
2023-03-06 13:32:28 +0200

    Delete cityArray.php

--- IVKHK/PHP/SchoolProject2023_Students/model/cityArray.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$cities = array(
-	array('id'=>1,'titel'=>'Tallinn', 'photo'=>'Tallinn.jpg'),
-	array('id'=>2,'titel'=>'Tartu', 'photo'=>'Tartu.jpg'),
-	array('id'=>3,'titel'=>'Toila', 'photo'=>'Toila.jpg'),
-);
